ed4b6a056e6640bda0fc33478ef08eb38a421087
Vincent GIRARD-REYDET <vincent.girard-reydet@socgen.com>
2019-08-07 15:01:59 +0200

    Configure deployment to Maven Central

---
+++ release/createReleaseProperties.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "scm.url=scm:git:git@github.com:societe-generale/sslr-yaml-parser.git" >> release.properties
+echo "scm.tag=$TRAVIS_TAG" >> release.properties
+
+echo "******release.properties content*******"
+cat release.properties
+
+echo "******directory content*******"
+pwd
+ls -al
